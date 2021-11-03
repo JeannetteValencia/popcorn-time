25e8738101576aab57bc1d07887758453a2c7808
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-03 18:08:44 +0100

    Step 5 Displaying classic movies with CSS Styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,37 @@
 import React from "react"
 import Movie from "./Movie"
 import moviesArr from "../data/movies.json"
-export function Main(){
+export class Main extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      moviesToDisplay: moviesArr
+    }
+  }
 
-  return (
-    <>
-    {moviesArr.map((movie)=>{
-      return <Movie key={movie.id} title={movie.title} year={movie.year} rating={movie.rating} imgURL={movie.imgURL}/>
-    })}
-
-    </>
-  )
+  renderClassicsOnly= ()=>{//arrow functions allow me to access this
+    this.setState((prevState, props)=>{
+      const newList= prevState.moviesToDisplay.filter((movie)=>{
+         return movie.year<2000
+      });
+      return {moviesToDisplay: newList};
+    })
+  }
+  render(){
+    return (
+      <>
+        <div className="controls">
+            <button onClick={this.renderClassicsOnly}>
+              Display only classics
+            </button>
+          </div>
+        <div className="movie-list">
+          {this.state.moviesToDisplay.map((movie)=>{
+            return <Movie key={movie.id} title={movie.title} year={movie.year} rating={movie.rating} imgURL={movie.imgURL}/>
+          })}
+  
+        </div>
+      </>
+    )
+  }
 }
