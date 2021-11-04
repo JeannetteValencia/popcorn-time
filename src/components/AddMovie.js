bd45274a525d97aa11c8dae14f6c3f81b74b3ede
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-04 12:44:54 +0100

    Functionality to add controlled component with multiple input fields

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -5,12 +5,15 @@ export default class AddMovie extends Component {
   constructor(props){
     super(props)
     this.state={
-      title: ""
+      title: "",
+      year: 1900,
+      rating: 0,
     }
   }
 
-  handleTitleChange = (event)=>{
-    this.setState({title: event.target.value})
+  handleInputChange =(event)=>{
+    const inputName = event.target.name;
+    this.setState({[inputName]: event.target.value})
   }
   render() {
     return (
@@ -19,7 +22,15 @@ export default class AddMovie extends Component {
         <form>
           <label>
             Title:
-            <input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange}></input>
+            <input type="text" name="title" min="1900" max="2050" value={this.state.title} onChange={this.handleInputChange}></input>
+          </label>
+          <label>
+            Year:
+            <input type="number" name="year" value={this.state.year} onChange={this.handleInputChange}></input>
+          </label>
+          <label>
+            Year:
+            <input type="number" name="rating" min="0" max="10" value={this.state.rating} onChange={this.handleInputChange}></input>
           </label>
            <button>Submit</button>
         </form>
